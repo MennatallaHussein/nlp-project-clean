08e0c408847a0fd048dce7d7ce20938219791361
Mennatalla <167652700+MennatallaHussein@users.noreply.github.com>
2025-09-05 01:52:01 +0300

    Update gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -64,13 +64,12 @@ def classify_text(text_classification_model,text_classification_data,text_to_cla
 
 
 def chat_with_character_chatbot(message, history):
-    character_Chatbot=CharacterChatBot("mennaGHANAM/Naruto-Llama-3-8B",
+    character_Chatbot=CharacterChatBot("AbdullahTarek/Naruto_Llama-3-8B",
                                         huggingface_token=os.getenv('huggingface_token'),
                                         )
-    output=character_Chatbot.chat(message,history)
-   
-    return output.strip()
-
+    
+    reply = character_Chatbot.chat(message, history)
+    return reply
 
 
 
@@ -153,3 +152,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
